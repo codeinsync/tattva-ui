7d699e279c116f44e1647d5b93be85d852d76585
Ankur Agarwal <ankur@Ankurs-MacBook-Air.local>
2020-08-24 22:02:43 +0530

    setup base storybook

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+}
