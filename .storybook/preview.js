bc3272ff85924a3ba8a554a2ac59d75cbf2a5cce
Ankur Agarwal <agarwalankur58@gmail.com>
2020-08-25 07:12:33 +0530

    basic badge component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import React from 'react';
+import Layout from "./Layout";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+const LayoutDecorator = (Story, context) => {
+  return (
+    <Layout>
+      <Story {...context} />
+    </Layout>
+  )
+}
+
+export const decorators = [LayoutDecorator];
